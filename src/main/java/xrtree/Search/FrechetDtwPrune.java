7046a20c5ef25bf0b56c90171b2d2e823f179d7f
HaniRamadhan <hani.its.042@gmail.com>
2021-10-18 05:28:27 +0900

    Fixed DFS of Frechet & DTW DP

--- src/main/java/xrtree/Search/FrechetDtwPrune.java
+++ src/main/java/xrtree/Search/FrechetDtwPrune.java
@@ -7,45 +7,36 @@ import java.util.Stack;
 
 public class FrechetDtwPrune {
     public static int dfsDpTable(int i, int j, BitSet[] matchTable, SubTrajTable stTable){
-        Stack<Integer> iStack, jStack;
-        iStack = new Stack<>();
-        jStack = new Stack<>();
-        iStack.add(i);
-        jStack.add(j);
-
-        BitSet visited = new BitSet();
+        int iMax = matchTable.length-1;
         int jMax = j;
-        int currentTid = stTable.getTidOfPtr(j);
-        while(!iStack.isEmpty()){
-            int iCurrent = iStack.pop();
-            int jCurrent = jStack.pop();
-            if(visited.get(iCurrent * stTable.getLength() + jCurrent))
-                continue;
-            visited.set(iCurrent * stTable.getLength() + jCurrent);
+        for(;jMax<stTable.getLength();jMax++)
+            if(stTable.getTidOfPtr(j)!=jMax){
+                jMax--;
+                break;
+            }
 
-            if(stTable.isLastSub(jCurrent) && iCurrent == matchTable.length-1)
-                return jCurrent;
-            if(jMax < jCurrent)
-                jMax = jCurrent;
-            if(jCurrent<stTable.getLength()-1) {
-                if (matchTable[iCurrent].get(jCurrent + 1) && currentTid == stTable.getTidOfPtr(jCurrent + 1)) {
-                    iStack.add(iCurrent);
-                    jStack.add(jCurrent + 1);
-                }
-            }
-            if(iCurrent<matchTable.length-1){
-                if (matchTable[iCurrent+1].get(jCurrent)) {
-                    iStack.add(iCurrent + 1);
-                    jStack.add(jCurrent);
-                }
-            }
-            if(jCurrent<stTable.getLength()-1 && iCurrent<matchTable.length-1) {
-                if (matchTable[iCurrent+1].get(jCurrent + 1) && currentTid == stTable.getTidOfPtr(jCurrent + 1)) {
-                    iStack.add(iCurrent + 1);
-                    jStack.add(jCurrent + 1);
-                }
+        for(int ii=i;ii<=iMax;ii++) {
+            for (int jj = j; jj <= jMax; jj++){
+                if(ii==i && jj==j)
+                    continue;
+                if(!matchTable[ii].get(jj))
+                    continue;
+
+                boolean value = false;
+                if(ii>i)
+                    value = value || matchTable[ii-1].get(jj);
+                if(jj>j)
+                    value = value || matchTable[ii].get(jj-1);
+                if(ii>i && jj>j)
+                    value = value || matchTable[ii-1].get(jj-1);
+                if(!value)
+                    matchTable[ii].clear(jj);
             }
         }
+
+        if(matchTable[iMax].get(jMax))
+            return jMax;
+
         return -jMax;
     }
     public static BitSet retrieveTidFromMatchTable(BitSet[] matchTable, SubTrajTable table,
